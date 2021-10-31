3f7bbf1f9bf7002f7c8acc5d84857f85788df109
developNY <sksldi0@gmail.com>
2021-10-31 13:53:27 -0400

    Add pytorch lightening

--- pytorch_script.py
+++ pytorch_script.py
@@ -1,6 +1,8 @@
 import torch
 from transformers import BertTokenizer
 import MyDataset
+from torch.utils.data import DataLoader
+import LitAutiEncoder
 
 #Data load
 dataset = load_dataset('csv', data_files={'train': r'C:\Users\sksld\OneDrive\Desktop\AIM\data\train_sent_emo.csv',
@@ -15,13 +17,20 @@ max_epochs = 100
 for i in dataset['train'].length:
     #Generator
     train_set = MyDataset(tokenizer(dataset['train']['Utterance'][i]), dataset['train']['Sr No.'][i]) #list_IDs, labels
-    train_generator = torch.utils.data.DataLoader(validation_set, **params)
+    train_generator = DataLoader(validation_set, **params)
 
     test_set = MyDataset(tokenizer(dataset['test']['Utterance'][i]), dataset['test']['Sr No.'][i])
-    test_generator = torch.utils.data.DataLoader(validation_set, **params)
+    test_generator = DataLoader(validation_set, **params)
 
     dev_set = MyDataset(tokenizer(dataset['dev']['Utterance'][i]), dataset['dev']['Sr No.'][i])
-    dev_generator = torch.utils.data.DataLoader(validation_set, **params)
+    dev_generator = DataLoader(validation_set, **params)
+
+# model
+model = LitAutoEncoder()
+
+# training
+trainer = pl.Trainer(gpus=4, precision=16, limit_train_batches=0.5)
+trainer.fit(model, train_loader, val_loader)
 
 
 # #loop over epochs
